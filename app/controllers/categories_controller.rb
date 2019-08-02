34661c8498d5df8f65a3979cb4b0c84c8f11ab1d
Tony Baker <this.tonybaker@gmail.com>
2019-08-02 18:01:36 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,7 +22,7 @@ class CategoriesController < ApplicationController
 
   def update
     category = Category.find(params[:id])
-    category.update(params.require(:category))
+    category.update(category_params(:name))
     redirect_to category_path(category)
   end
 
