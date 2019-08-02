e1d364c6976ab8d6f8b047c19c5b94b9eb2f634a
Tony Baker <this.tonybaker@gmail.com>
2019-08-02 17:59:57 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,4 +25,9 @@ class CategoriesController < ApplicationController
     category.update(params.require(:category))
     redirect_to category_path(category)
   end
+
+  private
+  category_params(*args)
+    params.require(:category).permit(*args)
+  end
 end
