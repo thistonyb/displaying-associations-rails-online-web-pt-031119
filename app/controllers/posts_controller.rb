8da374fb82642bb4179edc3591bf203fb84dc5c7
Tony Baker <this.tonybaker@gmail.com>
2019-08-02 18:05:50 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 
 	def update
 		post = Post.find(params[:id])
-		post.update(post_params(:title, :description)
+		post.update(post_params(:title, :description))
 		redirect_to post_path(post)
 	end
 
