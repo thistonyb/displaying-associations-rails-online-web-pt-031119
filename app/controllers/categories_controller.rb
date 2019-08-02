83b2edd32060c0e6e91bc84acbe046593c1b11bb
Tony Baker <this.tonybaker@gmail.com>
2019-08-02 18:06:08 +0000

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,7 +22,12 @@ class CategoriesController < ApplicationController
 
   def update
     category = Category.find(params[:id])
-    category.update(params.require(:category))
+    category.update(category_params(:name))
     redirect_to category_path(category)
+  end
+
+  private
+  def category_params(*args)
+    params.require(:category).permit(*args)
   end
 end
